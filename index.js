f67cf129c2d89e4de05a47c6422072d80c25f9b3
Jeff Seaman <mr.jeffrey.seaman@gmail.com>
2019-03-07 19:14:18 -0800

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * JsonExporter
  *
- * 
+ * A small library that takes in a JSON spec describing how to export a known JS object to a file or stream. 
  *
  * Author: Jeff Seaman
  * Date: March 7, 2019
